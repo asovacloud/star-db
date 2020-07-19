78cf8b930c5489ca682bad721e68904f2c215997
Andrew Sova <andrii.sova@tbwa-x.com>
2020-07-19 14:27:55 +0300

    Add componentDidCatch

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,25 +2,16 @@ import React, { Component } from 'react'
 
 import Header from '../header';
 import RandomPlanet from '../random-planet';
-import ItemList from '../item-list';
-import PersonDetails from '../person-details';
 import ErrorIndicator from "../error-indicator";
-
+import PeoplePage from "../people-page";
 
 import './app.css';
 
 export default class App extends Component {
 
   state = {
-    selectedPerson: 2,
     hasError: false
   };
-
-  onPersonSelected = id => {
-    this.setState({
-      selectedPerson: id,
-    })
-  }
 
   componentDidCatch() {
     this.setState({ hasError: true })
@@ -37,14 +28,7 @@ export default class App extends Component {
         <Header />
         <RandomPlanet />
   
-        <div className="row mb-2">
-          <div className="col-md-6">
-            <ItemList onItemSelected={this.onPersonSelected} />
-          </div>
-          <div className="col-md-6">
-            <PersonDetails personId = {this.state.selectedPerson} />
-          </div>
-        </div>
+        <PeoplePage />
   
       </div>
     );
---
+++ src/components/people-page/people-page.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+
+import ItemList from "../item-list";
+import PersonDetails from "../person-details";
+import ErrorIndicator from "../error-indicator/error-indicator";
+
+import "./people-page.css";
+
+export default class PeoplePage extends Component {
+
+  state = {
+    selectedPerson: 3,
+    hasError: false,
+  }
+
+  componentDidCatch() {
+    this.setState({
+      hasError: true,
+    });
+  }
+
+  onPersonSelected = id => {
+    this.setState({
+      selectedPerson: id,
+    })
+  }
+
+  render() {
+
+    if (this.state.hasError) {
+      return <ErrorIndicator />
+    }
+
+    return (
+      <div className="row mb-2">
+        <div className="col-md-6">
+          <ItemList onItemSelected={this.onPersonSelected} />
+        </div>
+        <div className="col-md-6">
+          <PersonDetails personId = {this.state.selectedPerson} />
+        </div>
+      </div>
+    );
+  }
+
+}
