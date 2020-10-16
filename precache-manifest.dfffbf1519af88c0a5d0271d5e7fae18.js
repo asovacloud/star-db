dc638256d7e86fa4bd1fe36bceb213cdbfa359dd
Andrew Sova <andrii.sova@tbwa-x.com>
2020-10-16 19:37:50 +0300

    Updates

---
+++ precache-manifest.dfffbf1519af88c0a5d0271d5e7fae18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e93adf99fdbf01616907675e9a011ed5",
+    "url": "/asovacloud/star-db.git/index.html"
+  },
+  {
+    "revision": "39950d82e893ae246aa0",
+    "url": "/asovacloud/star-db.git/static/css/main.957b5db9.chunk.css"
+  },
+  {
+    "revision": "b4bebcbf6f8296b9d492",
+    "url": "/asovacloud/star-db.git/static/js/2.ba2f3a10.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/asovacloud/star-db.git/static/js/2.ba2f3a10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39950d82e893ae246aa0",
+    "url": "/asovacloud/star-db.git/static/js/main.1cb932a1.chunk.js"
+  },
+  {
+    "revision": "8780370a18f58871edd2",
+    "url": "/asovacloud/star-db.git/static/js/runtime-main.4c74b010.js"
+  }
+]);
